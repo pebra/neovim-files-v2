3af719f23b01d653179550617039d4910922e6a4
pebra <self@pebra.net>
2023-06-19 13:53:37 -0400

    Remove vimdoc from treesitter

--- lua/plugin_configs/treesitter.lua
+++ lua/plugin_configs/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "sql", "scala", "ruby", "elixir", "rust", "typescript" },
+  ensure_installed = { "c", "lua", "vim", "query", "sql", "scala", "ruby", "elixir", "rust", "typescript" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
